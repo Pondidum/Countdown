b774d276dd9eba66ec14068386eb687e9abcc05e
andy <andy@andydote.co.uk>
2017-08-19 18:51:11 +0300

    initial commit: blank project

---
+++ src/Countdown/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Countdown
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
