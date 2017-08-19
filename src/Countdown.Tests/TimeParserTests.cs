be8922b8b9a22c70ea5d11903b8223dcf5445ed6
andy <andy@andydote.co.uk>
2017-08-19 20:19:29 +0300

    timeParser: basic implementation

---
+++ src/Countdown.Tests/TimeParserTests.cs
@@ -0,0 +1,19 @@
+﻿using Shouldly;
+using System;
+using Xunit;
+
+namespace Countdown.Tests
+{
+	public class TimeParserTests
+	{
+		[Theory]
+		[InlineData("2017/08/19 19:57", "20:30", "2017/08/19 20:30")]
+		public void When_parsing_time(DateTime current, string entered, DateTime expected)
+		{
+			var parser = new TimeParser(() => current);
+			var target = parser.Parse(entered);
+
+			target.ShouldBe(expected);
+		}
+	}
+}
---
+++ src/Countdown/TimeParser.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Countdown
+{
+	public class TimeParser
+	{
+		private readonly Func<DateTime> _now;
+
+		public TimeParser(Func<DateTime> now)
+		{
+			_now = now;
+		}
+
+		public DateTime Parse(string entered)
+		{
+			var span = TimeSpan.Parse(entered);
+			var now = _now();
+
+			return now
+				.Subtract(now.TimeOfDay)
+				.Add(span);
+		}
+	}
+}
